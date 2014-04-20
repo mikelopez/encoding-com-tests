9603c25eded05a60fa0442f8eb4eea4c4f48bce0
Mike Lopez <dev@scidentify.info>
2014-04-19 21:15:41 -0400

    modthetests

--- tests/base.py
+++ tests/base.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('..')
+sys.path.append('../../')
 from termprint import *
 import xmltodict
 import xmldict
--- tests/parsing/test_xmltodict.py
+++ tests/parsing/test_xmltodict.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('../')
 from base import *
 from xml_request import *
 
