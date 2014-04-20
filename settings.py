f37d118977c01195f29285758afab15153c7537f
Mike Lopez <dev@scidentify.info>
2014-04-20 18:50:25 -0400

    modified settings to include sample video filename

--- settings.py
+++ settings.py
@@ -4,6 +4,9 @@ ENCODING_API_USER, ENCODING_API_KEY = None, None
 # selected url
 ENCODING_API_URL = "manage.encoding.com:80"
 
+# video used in testst 
+SAMPLE_VIDEO = 'sample-video.mp4'
+
 try:
     from local_settings import *
 except ImportError:
